780d9b251ead39567daf81456f5a170b87be0a9e
Dominic McGinnis <Dominic.McGinnis@elliemae.com>
2023-08-03 09:52:23 -0700

    fixed the infinite looping load issue

--- src/components/DisplayImage.jsx
+++ src/components/DisplayImage.jsx
@@ -1,9 +1,9 @@
-export default ({image, handleImageChange}) => {
+export default ({image, disableNext, disablePrev, handleImageChange}) => {
     return (
         <hgroup>
-            <a className="outline" href="#" role="button" onClick={ () => { handleImageChange(Number(image.id - 1)) }} ><span className="material-symbols-outlined">arrow_back</span></a>
-            <img style={{ width: '200px' }} src={`images/${image.imageName}.jpg`} />
-            <a className="outline" href="#" role="button" onClick={ () => { handleImageChange(Number(image.id + 1)) }} ><span className="material-symbols-outlined">arrow_forward</span></a>
+            <a disabled={disablePrev} className="outline" href="#" role="button" onClick={ () => { handleImageChange(Number(image.id - 1)) }} ><span className="material-symbols-outlined">arrow_back</span></a>
+            <img style={{ width: '200px', height: '200px' }} src={`images/${image.imageName}.jpg`} />
+            <a disabled={disableNext} className="outline" href="#" role="button" onClick={ () => { handleImageChange(Number(image.id + 1)) }} ><span className="material-symbols-outlined">arrow_forward</span></a>
         </hgroup>
     )
 }
--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -3,28 +3,25 @@ import DisplayImage from './DisplayImage';
 
 
 export default () => {
-    const [isLoading, setisLoading] = useState(true);    
     const [ images, setImages ] = useState([]);
     let [ image, setImage ] = useState(null);
   
     const fetchImages = async () => {
-        setisLoading(true);
         let response = await fetch('images.json');
         let data = await response.json();
-        //setImages(data);
-        //setImage(data[0]);
-        setisLoading(false);
+        setImages(data);
+        setImage(data[0]);
     }
 
     useEffect(() => {
         fetchImages();
-    });
+    },[]);
 
     return (
         <div>
-            <article>
-            { isLoading || !image ?  ( <div className="loading content-loading" /> ) : (  <DisplayImage image={image} handleImageChange={ (info) => { /*setImage(images[info])*/ }} />) }
-            </article>
+            { (!image) ?  ( <div className="loading content-loading" /> ) : 
+                (  <DisplayImage image={image} disableNext={(image.id === images.length - 1)} disablePrev={(image.id === 0)} handleImageChange={ (info) => { setImage(images[info]) }} />) 
+            }
         </div>
     )
 }
